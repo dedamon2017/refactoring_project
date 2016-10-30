850b901ea2889d701bc185b56be0f004bbe4fa33
dedamon2016 <pavel.sharipov@gmail.com>
2016-10-30 12:35:33 +0200

    Extracted toInt method

--- refactoring-task1/src/main/java/it/discovery/refactoring/Calculator.java
+++ refactoring-task1/src/main/java/it/discovery/refactoring/Calculator.java
@@ -8,219 +8,197 @@ import java.io.IOException;
  * Calculation unit that implements arithmetic operations
  */
 public class Calculator {
-    private static final String ERROR_MESSAGE = "Oshibka";
-    public static String report = null;
+	private static final String ERROR_MESSAGE = "Oshibka";
+	public static String report = null;
 
-    // Primer ispolzovaniya: zapustit is IDE s argumentami
-    // 1) znak operacii
-    // 2) argument #1,
-    // 3) argument #2
-    // 4) sistema schisleniya - 2, 8, 10 ili 16 (opcionalno, default 10)
-    // 5) vyvod v konsol(false) ili file(true) (opcionalno)
-    // naprimer, + 2 2 true
-    public static void main(String[] argumenty) {
-        String operation = argumenty[0];
-        String firstOp = argumenty[1];
-        String secondOp = argumenty[2];
-        String scale = null;
-        try {
-            scale = argumenty[3];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            scale = "10";
-        }
+	// Primer ispolzovaniya: zapustit is IDE s argumentami
+	// 1) znak operacii
+	// 2) argument #1,
+	// 3) argument #2
+	// 4) sistema schisleniya - 2, 8, 10 ili 16 (opcionalno, default 10)
+	// 5) vyvod v konsol(false) ili file(true) (opcionalno)
+	// naprimer, + 2 2 true
+	public static void main(String[] argumenty) {
+		String operation = argumenty[0];
+		String firstOp = argumenty[1];
+		String secondOp = argumenty[2];
+		String scale = null;
+		try {
+			scale = argumenty[3];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			scale = "10";
+		}
 
-        String toConsole = null;
-        try {
-            toConsole = argumenty[4];
-        } catch (ArrayIndexOutOfBoundsException iskl) {
-            toConsole = "false";
-        }
+		String toConsole = null;
+		try {
+			toConsole = argumenty[4];
+		} catch (ArrayIndexOutOfBoundsException iskl) {
+			toConsole = "false";
+		}
 
-        calculate(operation, firstOp, secondOp, scale, toConsole);
-    }
+		calculate(operation, firstOp, secondOp, scale, toConsole);
+	}
 
-    /**
-     * Glavnaya function
-     *
-     * @param argumenty
-     */
-    public static int calculate(String operation, String firstOp, String secondOp, String scale, String console) {
-        report = "";
-        report = "";
-        report += "Nachali rabotu\n";
-//		String operaciya = argumenty[10];
-//		boolean probel = operaciya.equals("");
-//		if (probel) {
-//			operaciya = "+";
-//		}
+	/**
+	 * Glavnaya function
+	 *
+	 * @param argumenty
+	 */
+	public static int calculate(String operation, String firstOp, String secondOp, String scale, String console) {
+		report = "";
+		report = "";
+		report += "Nachali rabotu\n";
+		// String operaciya = argumenty[10];
+		// boolean probel = operaciya.equals("");
+		// if (probel) {
+		// operaciya = "+";
+		// }
 
+		boolean isConsole = isConsole(console);
+		try {
+			switch (operation) {
+			case "+": {
+				report += "Operaciya slojit\n";
+				int firstNumber = toInt(firstOp);
+				int secondNumber = toInt(secondOp);
 
-        boolean isConsole = isConsole(console);
+				report += "Argument 1 " + convertToScale(firstNumber, scale) + "\n";
+				report += "Argument 2 " + convertToScale(secondNumber, scale) + "\n";
+				int result = firstNumber + secondNumber;
+				report += "Rezultat " + convertToScale(result, scale) + "\n";
+				System.out.println(result);
+				try {
+					report += "Zakonchili rabotu\n";
+					if (isConsole) {
+						System.out.println(report);
+					} else {
+						saveToFile();
+					}
 
-        switch (operation) {
-            case "+": {
-                report += "Operaciya slojit\n";
-                int firstNumber = 0;
-                try {
-                    firstNumber = Integer.parseInt(firstOp);
-                } catch (Exception ex) {
-                    System.out.println("Oshibka");
-                    return -1;
-                }
-                int secondNumber = 0;
-                try {
-                    secondNumber = Integer.parseInt(secondOp);
-                } catch (Exception ex) {
-                    System.out.println("Oshibka");
-                    return -1;
-                }
+				} catch (Exception e) {
+					System.out.println("Oshibka");
+					return -1;
+				}
+			}
 
-                report += "Argument 1 " + convertToScale(firstNumber, scale) + "\n";
-                report += "Argument 2 " + convertToScale(secondNumber, scale) + "\n";
-                int result = firstNumber + secondNumber;
-                report += "Rezultat " + convertToScale(result, scale) + "\n";
-                System.out.println(result);
-                try {
-                    report += "Zakonchili rabotu\n";
-                    if (isConsole) {
-                        System.out.println(report);
-                    } else {
-                        saveToFile();
-                    }
+				break;
 
-                } catch (Exception e) {
-                    System.out.println("Oshibka");
-                    return -1;
-                }
-            }
+			case "-": {
+				report += "Operaciya vychitanie\n";
+				int firstNumber = toInt(firstOp);
+				int secondNumber = toInt(secondOp);
 
-            break;
+				report += "Argument 1 " + convertToScale(firstNumber, scale) + "\n";
+				report += "Argument 2 " + convertToScale(secondNumber, scale) + "\n";
+				int result = firstNumber - secondNumber;
+				report += "Rezultat " + convertToScale(result, scale) + "\n";
+				System.out.println(result);
+				try {
+					report += "Zakonchili rabotu\n";
+					if (isConsole) {
+						System.out.println(report);
+					} else {
+						saveToFile();
+					}
 
-            case "-": {
-                report += "Operaciya vychitanie\n";
-                int firstNumber = 0;
-                try {
-                    firstNumber = Integer.parseInt(firstOp);
-                } catch (Exception ex) {
-                    System.out.println("Oshibka");
-                    return -1;
-                }
-                int secondNumber = 0;
-                try {
-                    secondNumber = Integer.parseInt(secondOp);
-                } catch (Exception ex) {
-                    System.out.println(ERROR_MESSAGE);
-                }
+				} catch (Exception e) {
+					System.out.println(ERROR_MESSAGE);
+					return -1;
+				}
+			}
 
-                report += "Argument 1 " + convertToScale(firstNumber, scale) + "\n";
-                report += "Argument 2 " + convertToScale(secondNumber, scale) + "\n";
-                int result = firstNumber - secondNumber;
-                report += "Rezultat " + convertToScale(result, scale) + "\n";
-                System.out.println(result);
-                try {
-                    report += "Zakonchili rabotu\n";
-                    if (isConsole) {
-                        System.out.println(report);
-                    } else {
-                        saveToFile();
-                    }
+				break;
+			case "*": {
+				report += "Operaciya umnojeniya\n";
+				int firstNumber = toInt(firstOp);
+				int secondNumber = toInt(secondOp);
+				report += "Argument 1 " + convert(firstNumber, scale) + "\n";
+				report += "Argument 2 " + convert(secondNumber, scale) + "\n";
+				int result = firstNumber * secondNumber;
+				report += "Rezultat " + convert(result, scale) + "\n";
+				System.out.println(result);
+				try {
+					report += "Zakonchili rabotu\n";
+					if (isConsole) {
+						System.out.println(report);
+					} else {
+						saveToFile();
+					}
+				} catch (Exception e) {
+					System.out.println(ERROR_MESSAGE);
+					return -1;
+				}
 
-                } catch (Exception e) {
-                    System.out.println(ERROR_MESSAGE);
-                    return -1;
-                }
-            }
+			}
 
-            break;
-            case "*": {
-                report += "Operaciya umnojeniya\n";
-                int firstNumber = 0;
-                try {
-                    firstNumber = Integer.parseInt(firstOp);
-                } catch (Exception ex) {
-                    System.out.println(ERROR_MESSAGE);
-                }
-                int secondNumber = 0;
-                try {
-                    secondNumber = Integer.parseInt(secondOp);
-                } catch (Exception ex) {
-                    System.out.println(ERROR_MESSAGE);
-                }
-                report += "Argument 1 " + convert(firstNumber, scale) + "\n";
-                report += "Argument 2 " + convert(secondNumber, scale) + "\n";
-                int result = firstNumber * secondNumber;
-                report += "Rezultat " + convert(result, scale) + "\n";
-                System.out.println(result);
-                try {
-                    report += "Zakonchili rabotu\n";
-                    if (isConsole) {
-                        System.out.println(report);
-                    } else {
-                        saveToFile();
-                    }
-                } catch (Exception e) {
-                    System.out.println(ERROR_MESSAGE);
-                    return -1;
-                }
+				break;
+			}
+		} catch (Exception e) {
+			return -1;
+		}
+		return 0;
 
-            }
+	}
 
-            break;
-        }
-        return 0;
+	private static int toInt(String text) {
+		try {
+			return Integer.parseInt(text);
+		} catch (NumberFormatException ex) {
+			System.out.println("Oshibka");
+			throw new RuntimeException(ex);
+		}
 
-    }
+	}
 
-    private static boolean isConsole(String console) {
-        boolean isConsole = false;
-        if (console != null && console.equals("true")) {
-            isConsole = true;
-        }
-        return isConsole;
-    }
+	private static boolean isConsole(String console) {
+		boolean isConsole = false;
+		if (console != null && console.equals("true")) {
+			isConsole = true;
+		}
+		return isConsole;
+	}
 
-    private static void saveToFile() throws IOException {
-        FileWriter writer = new FileWriter("d:\\test.txt");
-        BufferedWriter out = new BufferedWriter(writer);
-        out.write(report);
-        out.close();
-    }
+	private static void saveToFile() throws IOException {
+		FileWriter writer = new FileWriter("d:\\test.txt");
+		BufferedWriter out = new BufferedWriter(writer);
+		out.write(report);
+		out.close();
+	}
 
-    private static String convertToScale(int value, String scale) {
-        switch (scale) {
-            case "2":
-                return Integer.toBinaryString(value);
-            case "8":
-                return Integer.toOctalString(value);
-            case "16":
-                return Integer.toHexString(value);
-            default:
-                return "" + value;
-        }
-    }
+	private static String convertToScale(int value, String scale) {
+		switch (scale) {
+		case "2":
+			return Integer.toBinaryString(value);
+		case "8":
+			return Integer.toOctalString(value);
+		case "16":
+			return Integer.toHexString(value);
+		default:
+			return "" + value;
+		}
+	}
 
-    private static String convert(int data, String schis) {
-        int defaultScale = 10;
-        try {
-            defaultScale = Integer.parseInt(schis);
-        } catch (NumberFormatException iskl) {
+	private static String convert(int data, String schis) {
+		int defaultScale = 10;
+		try {
+			defaultScale = Integer.parseInt(schis);
+		} catch (NumberFormatException iskl) {
 
-        }
+		}
 
-
-        return Integer.toString(data, defaultScale);
-    }
+		return Integer.toString(data, defaultScale);
+	}
 
 }
 
-//class OtchetTools {
-//	public static void zapusk() {
-//		String report = Calculator.report;
-//		System.out.println(report);
-//	}
+// class OtchetTools {
+// public static void zapusk() {
+// String report = Calculator.report;
+// System.out.println(report);
+// }
 //
-//	public static void main(String[] argumenty) {
-//		OtchetTools report = new OtchetTools();
-//		report.zapusk();
-//	}
-//}
+// public static void main(String[] argumenty) {
+// OtchetTools report = new OtchetTools();
+// report.zapusk();
+// }
+// }
